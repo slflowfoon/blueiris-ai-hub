cac6c1208cc88b25ed06fc41ddfcd2063262e4d2
slflowfoon <94804320+slflowfoon@users.noreply.github.com>
2026-04-07 22:36:58 +0100

    Add pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,19 @@
 import pytest
 import os
 import tempfile
-from app.wsgi import app, init_db
+from app import wsgi
 
 @pytest.fixture
 def client():
+    """Configures the app for testing with a temporary database."""
     db_fd, db_path = tempfile.mkstemp()
-    app.config['TESTING'] = True
     
-    import app.wsgi
-    app.wsgi.DB_FILE = db_path
+    wsgi.app.config['TESTING'] = True
+    wsgi.DB_FILE = db_path
     
-    with app.test_client() as client:
-        with app.app_context():
-            init_db() 
+    with wsgi.app.test_client() as client:
+        with wsgi.app.app_context():
+            wsgi.init_db() 
         yield client
 
     os.close(db_fd)
