8a5412a33f223c25704b18264e6e05c7f3e06597
slflowfoon <94804320+slflowfoon@users.noreply.github.com>
2026-04-08 09:36:03 +0100

    Update app/wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -817,4 +817,5 @@ def webhook(config_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
